56f3ce7c3111a3594d83339839918dac4e96c468
hanchang97 <nimok97@gmail.com>
2022-02-21 00:07:08 +0900

    feat : 권한 요청 기능 study

--- selfStudy/Gallery/app/src/main/java/com/example/gallery_study/MainActivity.kt
+++ selfStudy/Gallery/app/src/main/java/com/example/gallery_study/MainActivity.kt
@@ -1,22 +1,23 @@
 package com.example.gallery_study
 
+import android.Manifest
 import android.app.Activity
-import android.app.Instrumentation
 import android.content.Intent
 import android.graphics.Bitmap
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.MediaStore
 import android.util.Log
 import androidx.activity.result.contract.ActivityResultContracts
 import com.example.gallery_study.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
+
 
 const val REQUEST_IMAGE_GET = 100
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding : ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,25 +28,27 @@ class MainActivity : AppCompatActivity() {
 
         setButton1()
         setButton2()
-        setButton3()
+        setButton3() // 앱 선택기 직접
+        setButton4() // 권한 요청
     }
 
-    fun setButton1(){
+    fun setButton1() {
         binding.btn1.setOnClickListener {
             selectImage1()
         }
     }
 
-    fun setButton2(){  // registerForActivityResult를 버튼의 setOnClickListener 안에서 호출 하면 오류 발생!! 초기화 시점이 중요한 듯
-        var getContent = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
-            if(it.resultCode == RESULT_OK){
-                Log.d("AppTest", "data : ${it.data}")
-                binding.iv.setImageURI(it.data?.data)
+    fun setButton2() {  // registerForActivityResult를 버튼의 setOnClickListener 안에서 호출 하면 오류 발생!! 초기화 시점이 중요한 듯
+        var getContent =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()
+            ) {
+                if (it.resultCode == RESULT_OK) {
+                    Log.d("AppTest", "data : ${it.data}")
+                    binding.iv.setImageURI(it.data?.data)
+                } else {
+                    Log.d("AppTest", "btn2 사진 선택 x")
+                }
             }
-            else{
-                Log.d("AppTest", "btn2 사진 선택 x")
-            }
-        }
 
         binding.btn2.setOnClickListener {
             val intent = Intent(Intent.ACTION_PICK) // 갤러리로 이동
@@ -59,7 +62,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun selectImage1(){
+    fun selectImage1() {
         val intent = Intent(Intent.ACTION_PICK).apply {
             type = "image/*"
         }
@@ -79,13 +82,12 @@ class MainActivity : AppCompatActivity() {
             // Do work with photo saved at fullPhotoUri
             Log.d("AppTest", "photo uri : $fullPhotoUri")
             binding.iv.setImageURI(fullPhotoUri)
-        }
-        else{
+        } else {
             Log.d("AppTest", "사진 선택 x")
         }
     }
 
-    fun setButton3(){
+    fun setButton3() {
         //val sendIntent = Intent(Intent.ACTION_SEND)
         //val sendIntent = Intent(Intent.ACTION_GET_CONTENT)
         val sendIntent = Intent(Intent.ACTION_PICK)
@@ -98,10 +100,26 @@ class MainActivity : AppCompatActivity() {
             if (sendIntent.resolveActivity(packageManager) != null) {
                 //startActivity(chooser)
                 startActivityForResult(chooser, REQUEST_IMAGE_GET)
-            }
-            else{
+            } else {
                 Log.d("AppTest", "nothing to execute")
             }
+        }
+    }
+
+    fun setButton4() {
+        val requestPermissionLauncher =
+            registerForActivityResult(ActivityResultContracts.RequestPermission()
+            ) { isGranted: Boolean ->
+                if (isGranted) {
+                    Log.d("AppTest", "권한 승인 ok")
+                } else {
+                    Snackbar.make(binding.root, "권한이 승인되지 않았습니다", Snackbar.LENGTH_SHORT ).show()
+                }
+            }
+
+        binding.btn4.setOnClickListener {
+            requestPermissionLauncher.launch(
+                Manifest.permission.READ_EXTERNAL_STORAGE)
         }
     }
 
