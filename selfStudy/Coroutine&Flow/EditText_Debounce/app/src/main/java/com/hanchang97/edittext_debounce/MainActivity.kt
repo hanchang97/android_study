1d42fe2c536396b0f61734134e5650308dd12c34
hanchang97 <nimok97@gmail.com>
2022-06-02 16:50:48 +0900

    [UI_Study] [PriceRangeBar] left, right pin 변경에 따라 값 받아오기 구현

---
+++ selfStudy/Coroutine&Flow/EditText_Debounce/app/src/main/java/com/hanchang97/edittext_debounce/MainActivity.kt
@@ -0,0 +1,87 @@
+package com.hanchang97.edittext_debounce
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import android.view.View
+import android.widget.EditText
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val editText = findViewById<EditText>(R.id.editText)
+
+        /*editText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                lifecycleScope.launch {
+                    flow {
+                        emit(s.toString())
+                    }.debounce(1000L).collect {
+                        Log.d("AppTest", "debounce: ${it}")
+                    }
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                //Log.d("AppTest", "text len : ${s.toString().length}")
+            }
+
+        })*/
+
+        editText.textChangesToFlow().debounce(1500)
+            .onEach {
+                Log.d("AppTest", "debounce : ${it.toString()}")
+            }.launchIn(lifecycleScope)
+
+
+    }
+
+    fun EditText.textChangesToFlow(): Flow<CharSequence?> {
+        // flow 콜백 받기
+        return callbackFlow<CharSequence?> {
+            val listener = object : TextWatcher {
+                override fun beforeTextChanged(
+                    s: CharSequence?,
+                    start: Int,
+                    count: Int,
+                    after: Int
+                ) {
+                    Unit
+                }
+
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+                    // 값 내보내기
+                    trySend(s).isSuccess
+                }
+
+                override fun afterTextChanged(s: Editable?) {
+                    Unit
+                }
+            }
+
+            // 리스너 달아주기
+            addTextChangedListener(listener)
+
+            // 콜백이 사라질 때 실행됨
+            awaitClose {
+                removeTextChangedListener(listener)
+            }
+
+        }.onStart {
+            emit(text)  // EidtText의 getText
+        }
+    }
+}
--- selfStudy/UI_Study/PriceRangeBar/app/src/main/java/com/hanchang97/rangebarstudy/MainActivity.kt
+++ selfStudy/UI_Study/PriceRangeBar/app/src/main/java/com/hanchang97/rangebarstudy/MainActivity.kt
@@ -2,6 +2,8 @@ package com.hanchang97.rangebarstudy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.android.material.textfield.TextInputEditText
 import com.stfalcon.pricerangebar.RangeBarWithChart
 import com.stfalcon.pricerangebar.model.BarEntry
 import java.util.ArrayList
@@ -13,14 +15,110 @@ class MainActivity : AppCompatActivity() {
 
         val rangeBarWithChart = findViewById<RangeBarWithChart>(R.id.rangeBarWithChart)
 
-        val array = arrayOf(
-            10.0f to  0.0f, 15.0f to  10.0f, 20.0f to 15.0f, 25.0f to 20.0f, 30.0f to 25.0f, 35.0f to 30.0f,
-        ).map { BarEntry(it.first, it.second) }
+        val etLeftPin = findViewById<TextInputEditText>(R.id.tiet_leftpin)
+        val etRightPin = findViewById<TextInputEditText>(R.id.tiet_rightpin)
 
+        val priceList = ArrayList<Int>()
+        priceList.add(18000)
+        priceList.add(33000)
+        priceList.add(15000)
+        priceList.add(45000)
+        priceList.add(20000)
+        priceList.add(37000)
+        priceList.add(37000)
+        priceList.add(22000)
+        priceList.add(26000)
+
+        val chartList = ArrayList<BarEntry>()
+
+        var point = 0f
+
+
+      /*  priceList.forEach {
+            chartList.add(BarEntry(point, 0f))
+            chartList.add(BarEntry(point, it.toFloat()))
+            point += 5f
+
+            chartList.add(BarEntry(point, it.toFloat()))
+            chartList.add(BarEntry(point, 0f))
+            point += 5f
+        }*/
+
+        //chartList.add(BarEntry(10000f, 0f))
+
+       /* chartList.add(BarEntry(10000f, 0f))
+        chartList.add(BarEntry(10000f, 3f))
+        chartList.add(BarEntry(19000f, 3f))
+        chartList.add(BarEntry(19000f, 0f))
+
+        chartList.add(BarEntry(20000f, 0f))
+        chartList.add(BarEntry(20000f, 5f))
+        chartList.add(BarEntry(29000f, 5f))
+        chartList.add(BarEntry(29000f, 0f))
+
+        chartList.add(BarEntry(30000f, 0f))
+        chartList.add(BarEntry(30000f, 4f))
+        chartList.add(BarEntry(39000f, 4f))
+        chartList.add(BarEntry(39000f, 0f))
+
+        chartList.add(BarEntry(40000f, 0f))
+        chartList.add(BarEntry(40000f, 7f))
+        chartList.add(BarEntry(49000f, 7f))
+        chartList.add(BarEntry(49000f, 0f))
+
+        chartList.add(BarEntry(50000f, 0f))
+        chartList.add(BarEntry(50000f, 5f))
+        chartList.add(BarEntry(59000f, 5f))
+        chartList.add(BarEntry(59000f, 0f))
+
+        chartList.add(BarEntry(60000f, 0f))
+        chartList.add(BarEntry(60000f, 10f))
+        chartList.add(BarEntry(69000f, 10f))
+        chartList.add(BarEntry(69000f, 0f))
+
+        chartList.add(BarEntry(70000f, 0f))
+        chartList.add(BarEntry(70000f, 4f))
+        chartList.add(BarEntry(79000f, 4f))
+        chartList.add(BarEntry(79000f, 0f))*/
+
+        for(i in 1..20){
+            val end = i * 50000f - 3000f
+            val start = end - 44000f
+
+            val count = (1..30).random().toFloat()
+
+            chartList.add(BarEntry(start, 0f))
+            chartList.add(BarEntry(start, count))
+            chartList.add(BarEntry(end, count))
+            chartList.add(BarEntry(end, 0f))
+
+        }
+
+        rangeBarWithChart.setEntries(chartList)
+
+
+       /* val array = arrayOf(
+           0.0f to 5.0f, 1.0f to 5.0f, 1.0f to 0.0f, 2.0f to 0.0f, 2.0f to 5.0f, 3.0f to 5.0f
+        ).map { BarEntry(it.first, it.second) }
+        // 위 같이 데이터 넣으면 왜 오류????
 
         val arrList = ArrayList<BarEntry>()
         arrList.addAll(array)
 
-        rangeBarWithChart.setEntries(arrList)
+        rangeBarWithChart.setEntries(arrList)*/
+
+        rangeBarWithChart.onLeftPinChanged = { index, leftPinValue ->
+            Log.d("AppTest", "leftpin / index : ${index}, value : ${leftPinValue}")
+            etLeftPin.setText(leftPinValue.toString())
+        }
+        rangeBarWithChart.onRightPinChanged = { index, rightPinValue ->
+            Log.d("AppTest", "rightpin / index : ${index}, value : ${rightPinValue}")
+            etRightPin.setText(rightPinValue.toString())
+        }
+
+
+
     }
+
+
 }
